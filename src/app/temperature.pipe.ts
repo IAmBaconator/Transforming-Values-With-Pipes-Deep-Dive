4794bc4f8eefa098c8fcd52f5994e2866774768a
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-05 23:24:47 +0000

    Update custom pipe to take arguments and convert to a  temperature formate based on arguments entered.

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -5,7 +5,8 @@ import { Pipe, PipeTransform } from "@angular/core";
     standalone: true
 })
 export class TemperaturePipe implements PipeTransform {
-    transform(value: string | number) {
+    transform(value: string | number, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
+        // First argument (required) to accept string or number to convert to number.
         let val: number;
 
         if(typeof value === 'string') {
@@ -13,9 +14,28 @@ export class TemperaturePipe implements PipeTransform {
         } else {
             val = value;
         }
+        // Second argument (required) accepts either 'cel' or 'fah' to determine the temperature type.
+        // Third argument (optional) accepts either 'cel' or 'fah' to convert arg1 to arg3 tempurater type.
+        let outputTemp: number;
 
-        const outputTemp = val * (9 / 5) + 32;
+        if(inputType === 'cel' && outputType === 'fah') {
+            outputTemp = val * (9 / 5) + 32;
+        } else if (inputType === 'fah' && outputType === 'cel') {
+            outputTemp = (val - 32) * (5 / 9);
+        } else {
+            outputTemp = val;
+        }
+        // Adds the appropriate temperature symbol after the converted number.
+        let symbol: '°C' | '°F';
 
-        return `${outputTemp} °F`
+        if (!outputType) {
+            symbol = inputType === 'cel' ? '°C' : '°F';
+        } else {
+            symbol = outputType === 'cel' ? '°C' : '°F';
+        }
+
+        //const outputTemp = val * (9 / 5) + 32;
+
+        return `${outputTemp} ${symbol}`;
     }
 }
